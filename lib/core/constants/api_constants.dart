1b67b933dc8e6dcaf7f745063ebf1598cbee4d0b
Jandson Barros <jandsonbarros87@gmail.com>
2026-06-14 20:20:43 -0300

    api

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  //static const String baseUrl = 'https://back-shalon.vercel.app/api';  
-  static const String baseUrl = 'http://localhost:8080/api';
+  static const String baseUrl = 'https://back-shalon.vercel.app/api';  
+  //static const String baseUrl = 'http://localhost:8080/api';
   static const String login = '$baseUrl/login';
   static const String produtos = '$baseUrl/produtos';
 }
